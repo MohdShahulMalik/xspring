94e21be3f4a8c22d807b2ba696d099c619356fdf
MohdShahulMalik <mohdshahulmalik@gmail.com>
2025-09-07 14:19:39 +0000

    Updated command configs

--- src/cli/commands/mod.rs
+++ src/cli/commands/mod.rs
@@ -1,9 +1,29 @@
-use clap::Subcommand;
+use clap::{ArgAction, Subcommand};
 
 #[derive(Subcommand)]
 pub enum Commands{
     Quick{
-        #[arg(short = 'n', long)]
-        name: String,
+        #[arg(short = 'm', long, action = ArgAction::SetTrue)]
+        maven: bool,
+
+        #[arg(short = 'e', long, action = ArgAction::SetTrue)]
+        extended: bool,
+    },
+
+    List {
+        #[arg(short = 'j', long, action = ArgAction::SetTrue)]
+        java: bool,
+
+        #[arg(short = 'b', long, action = ArgAction::SetTrue)]
+        boot: bool,
+
+        #[arg(short = 't', long = "type", action = ArgAction::SetTrue)]
+        project_type: bool,
+
+        #[arg(short = 'l', long, action = ArgAction::SetTrue)]
+        language: bool,
+
+        #[arg(short = 'd', long, action = ArgAction::SetTrue)]
+        deps: bool,
     }
 }
